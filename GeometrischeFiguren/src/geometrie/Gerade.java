be8e455c3c0fd06fa1646450d2303a26068ff2e1
CH10537 <CH10537@DELIN-V-0118504.sig.dom>
2021-02-09 21:54:27 +0100

    Implemented class Gerade

--- GeometrischeFiguren/src/geometrie/Gerade.java
+++ GeometrischeFiguren/src/geometrie/Gerade.java
@@ -41,14 +41,12 @@ public class Gerade
 	
 	public boolean istLaengerAls (Gerade g2)
 	{
-		return false;
+		return (this.laenge() > g2.laenge());
 	}
 	
 	public boolean equals (Gerade g2)
 	{
-		return false;
+		//return (this.equals(g2));	// geht das auch ??
+		return ( (g2.getStartPos().equals(this.startPos) && g2.getEndPos().equals(this.endPos)) || (g2.getStartPos().equals(this.endPos) && g2.getEndPos().equals(this.startPos)) ); 
 	}
-	
-	
-
 }
